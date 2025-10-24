e5f7ad4286fc026c881865dbb5c99d5207dac973
mukmibulat <mukmbulat07@gmail.com>
2025-10-24 19:42:22 +0300

    4 prac

--- 4py/2nd.py
+++ 4py/2nd.py
@@ -10,8 +10,6 @@ point=0
 K=0
 
 for i in range(len(text)):
-    if text[i]=='.':
-        point+=1
     if text[i]==',' or text[i]=='.' or text[i]==', ':
         if len(word)>K:
             K=len(word)
@@ -22,11 +20,7 @@ for i in range(len(text)):
 
 transformered_string=''
 
-if point==0:
-    print('This text does not contain a period! You must rewrite the text, adding only a single period at the end.')
-elif point==2:
-    print('This text contains more than two periods! You must rewrite the text, adding only a single period at the end.')
-elif len(code_word)<=20:
+if len(code_word)<=20:
     for symbol in text:
         if symbol in down_letters:
             for j in range(len(down_letters)//2):
@@ -37,7 +31,7 @@ elif len(code_word)<=20:
                 if [j]==symbol:
                     transformered_string+=up_letters[j+K]
         else:
-            transformered_string+=s
+            transformered_string+=symbol
     print(transformered_string,K)
 else:
     print('The maximum number of characters allowed is 20! Please re-enter the text.')
